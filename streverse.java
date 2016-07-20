8886ef13a14bc59b0e3dd880463f8ca813774b32
sharmila2612 <sharmila.aece@ksrce.ac.in>
2016-07-20 12:37:59 +0530

    Create streverse.java

---
+++ streverse.java
@@ -0,0 +1,9 @@
+public class streverse {
+
+	
+	public static void main(String[] args) {
+		String str="";
+		String s=new StringBuffer(args[0]).reverse().toString();
+		System.out.println("The reverse String is "+s);
+		}
+	}
