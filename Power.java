00ae05f89af4b90dae90ff1b0e63e31f4630ed5f
sharmila2612 <sharmila.aece@ksrce.ac.in>
2016-08-07 23:32:21 -0700

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Power{
+public static void main(String[] args){
+ Scanner in=new Scanner(System .in);
+ int n=in.nextInt();
+ int r=0;
+ int s=0,x=0;
+ while(n!=0)
+ {
+  r=n%10;
+  s=s+(int)Math.pow(r,x);
+  n=n/10;
+  x=r;
+ }
+ System.out.print(s)
+}
+}
